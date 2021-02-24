535e86f3c8e8a93c8002990d77b0e71cde446cb8
Teangelo Burks <burks.teangelo@gmail.com>
2021-02-23 22:23:03 -0500

    Data for note text/title is now displaying in the console. Have to work on getting information to show on the page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,24 @@
-const noteData = require('../Develop/db/db.json');
+const noteText = require('../Develop/db/db.json');
 const fs = require('fs')
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(noteData))
+    app.get('/api/notes', (req, res) => res.json(noteText));
 
 
-    app.post('/api/notes', (req, res) => res.json(noteData))
+    app.post('/api/notes', (req, res) => {
+        const newTitle = req.body;
+
+        noteText.push(newTitle);
+        console.log(noteText);
+        // res.json(newTitle);
+
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(noteText, null, 2), (err) => {
+            if (err) throw err;
+            console.log("error");
+        });
+        // console.log("new note");
+        // return res.json(newTitle);
+        res.end();
+    });
 
 }
